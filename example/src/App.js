fded10e2a7d5503f8760b267b58b8b2df02e174d
madarsbiss <madars.biss@gmail.com>
2021-03-18 19:46:43 +0200

    added syntax highlighter

--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,117 @@
 import React from 'react'
 import styles from './App.module.css'
 
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
 import { ExampleComponent } from 'image-slideshow'
 import 'image-slideshow/dist/index.css'
 
 const App = () => {
-
   return (
     <div className={styles.wrapper}>
       <h1>Image Slider Component</h1>
       <p>This is the description of the component</p>
       <ExampleComponent text='Create React Library Example 😄' />
+      <h1>Installation</h1>
+      <SyntaxHighlighter language='bash' style={coldarkDark}>
+        {`npm install @madzadev/audio-player`}
+      </SyntaxHighlighter>
+      <h1>Usage</h1>
+      <SyntaxHighlighter language='javascript' style={coldarkDark}>
+        {`import Player from '@madzadev/audio-player'`}
+      </SyntaxHighlighter>
+      <SyntaxHighlighter language='javascript' style={coldarkDark}>
+        {`const tracks = [
+          {
+            url: 'https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3',
+            title: 'Madza - Chords of Life',
+            tags: ['house']
+          },
+          {
+            url: 'https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3',
+            title: 'Madza - Late Night Drive',
+            tags: ['dnb']
+          },
+          {
+            url: 'https://audioplayer.madza.dev/Madza-Persistence.mp3',
+            title: 'Madza - Persistence',
+            tags: ['dubstep']
+          }
+        ]`}
+      </SyntaxHighlighter>
+      <SyntaxHighlighter language='jsx' style={coldarkDark}>
+        {`<Player trackList={tracks} />`}
+      </SyntaxHighlighter>
+      <p className='warning'>
+        <code>'trackList'</code> is the mandatory prop and requires to pass in
+        an array consisting of objects with <code>url</code>, <code>title</code>{' '}
+        and <code>tags</code> keys.
+      </p>
+      <h1>Options</h1>
+      <p className='note'>
+        The default values of available options props are displayed.
+      </p>
+      <SyntaxHighlighter language='jsx' style={coldarkDark}>
+        {`<Player
+        trackList={tracks}
+        includeTags={true}
+        includeSearch={true}
+        showPlaylist={true}
+        autoPlayNextTrack={true}
+      />`}
+      </SyntaxHighlighter>
+      {/* <h1>Features</h1>
+      Play/Pause Next/Previous tracks Loop audio Shuffle play Drag progress bar
+      Volume control Clickable playlist Filter audio files based on genre Search
+      audio files by title Responsive design */}
+      <h1>Color schemas</h1>
+      <p className='note'>The player uses 5 color palette by default.</p>
+      <p className='warning'>
+        You can further customize the player by editing the colors variable
+        below.
+      </p>
+      <SyntaxHighlighter language='javascript' style={coldarkDark}>
+        {`const colors = \`html {
+          --tagsBackground: #9440f3;
+          --tagsText: #ffffff;
+          --tagsBackgroundHoverActive: #2cc0a0;
+          --tagsTextHoverActive: #ffffff;
+          --searchBackground: #18191f;
+          --searchText: #ffffff;
+          --searchPlaceHolder: #575a77;
+          --playerBackground: #18191f;
+          --titleColor: #ffffff; 
+          --timeColor: #ffffff;
+          --progressSlider: #9440f3;
+          --progressUsed: #ffffff;
+          --progressLeft: #151616;
+          --volumeSlider: #9440f3;
+          --volumeUsed: #ffffff;
+          --volumeLeft:  #151616;
+          --playlistBackground: #18191f;
+          --playlistText: #575a77;
+          --playlistBackgroundHoverActive:  #18191f;
+          --playlistTextHoverActive: #ffffff;
+      }\``}
+      </SyntaxHighlighter>
+      <SyntaxHighlighter language='jsx' style={coldarkDark}>
+        {`<Player
+        trackList={tracks}
+        customColorScheme={colors}
+      />`}
+      </SyntaxHighlighter>
+      <h1>Final notes</h1>
+      <p className='warning'>
+        It's recommended to use CMS like a Contentful or DatoCMS to manage your
+        audio files and access them via API.
+      </p>
+      <p className='note'>
+        Other alternatives include Google Drive and Dropbox or store you audio
+        files directly in the project.
+      </p>
     </div>
   )
-  
-  
 }
 
 export default App
