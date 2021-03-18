8ca9260a2e6046b5adc90151a4340889c80b173c
madarsbiss <madars.biss@gmail.com>
2021-03-18 19:32:11 +0200

    init commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import styles from './App.module.css'
 
 import { ExampleComponent } from 'image-slideshow'
 import 'image-slideshow/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+  return (
+    <div className={styles.wrapper}>
+      <h1>Image Slider Component</h1>
+      <p>This is the description of the component</p>
+      <ExampleComponent text='Create React Library Example 😄' />
+    </div>
+  )
+  
+  
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.css'
 
+const images = [
+  'https://source.unsplash.com/random/800x400?sig=1',
+  'https://source.unsplash.com/random/800x400?sig=2',
+  'https://source.unsplash.com/random/800x400?sig=3'
+]
+
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  // const [array, setArray] = useState([1, 2, 3])
+  let [active, setActive] = useState(0)
+
+  const leftClickHandle = () => {
+    console.log('Left clicked')
+    if (active !== 0) {
+      setActive((active -= 1))
+    } else {
+      setActive((active = images.length - 1))
+    }
+
+    // const [first, second, ...rest] = array
+    // setArray([...rest, first, second])
+  }
+
+  const rightClickHandle = () => {
+    console.log('Right clicked')
+    if (active !== images.length - 1) {
+      setActive((active += 1))
+    } else {
+      setActive((active = 0))
+    }
+
+    console.log(active)
+    // const [first, ...rest] = array
+    // setArray([...rest, first])
+  }
+
+  const dotClickHandler = (e) => {
+    const dotNum = e.target.getAttribute('data-key')
+    setActive((active = parseInt(dotNum)))
+  }
+  return (
+    <div>
+      <div className={styles.wrapper}>
+        <div className={styles.leftClick} onClick={leftClickHandle}>
+          ←
+        </div>
+        <img src={images[active]} alt='image' />
+        <div className={styles.rightClick} onClick={rightClickHandle}>
+          →
+        </div>
+      </div>
+      <div className={styles.dots}>
+        {images.map((el, index) => {
+          if (index !== active) {
+            return (
+              <div
+                className={styles.dot}
+                data-key={index}
+                onClick={dotClickHandler}
+              ></div>
+            )
+          } else {
+            return <div className={styles.activeDot}></div>
+          }
+        })}
+      </div>
+    </div>
+  )
+  // return <div className={styles.test}>Example Component: {text}</div>
 }
