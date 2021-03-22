f2c1793ddf2fe5924ad02909da764589306e2948
madarsbiss <madars.biss@gmail.com>
2021-03-22 18:37:54 +0200

    update svg buttons

--- example/src/App.js
+++ example/src/App.js
@@ -1,73 +1,50 @@
-import React from 'react'
-import styles from './App.module.css'
+import React from "react";
+import styles from "./App.module.css";
 
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-import { Slider } from 'image-slider'
-import 'image-slider/dist/index.css'
+import { Slider } from "image-slider";
+import "image-slider/dist/index.css";
 
 const images = [
   {
-<<<<<<< HEAD
-    url: 'https://source.unsplash.com/random/800x400?sig=1',
-    title:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-=======
-    url: './1.jpg',
-    title: 'AAAA',
->>>>>>> 02e91dd7fd48ddc04aebe3f2d2c093ef2e77225e
-    author: 'John Doe',
-    authorLink: 'dfdfrf'
+    url: "./1.jpg",
+    title: "AAAA",
+    author: "John Doe",
+    authorLink: "dfdfrf",
   },
   {
-<<<<<<< HEAD
-    url: 'https://source.unsplash.com/random/800x400?sig=2',
-    title:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-=======
-    url: './2.jpg',
-    title: 'BBBB',
->>>>>>> 02e91dd7fd48ddc04aebe3f2d2c093ef2e77225e
-    author: 'John Doe',
-    authorLink: 'dfdfrf'
+    url: "./2.jpg",
+    title: "BBBB",
+
+    author: "John Doe",
+    authorLink: "dfdfrf",
   },
   {
-    url: 'https://source.unsplash.com/random/800x400?sig=3',
+    url: "https://source.unsplash.com/random/800x400?sig=3",
     title:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    author: 'John Doe',
-    authorLink: 'dfdfrf'
-  }
-]
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    author: "John Doe",
+    authorLink: "dfdfrf",
+  },
+];
 
 const App = () => {
   return (
     <div className={styles.wrapper}>
       <h1>Image Slider Component</h1>
       <p>This is the description of the component</p>
-<<<<<<< HEAD
-      <Slider
-        imageList={images}
-        showCredits={true}
-        showProgress={true}
-        showControls={true}
-        showDescription={true}
-      />
+      <Slider imageList={images} autoPlay={true} showProgress={true} />
       <h1>Installation</h1>
-=======
-      {/* <div style={{ width: '800px', height: '400px' }}> */}
-      <Slider imageList={images} autoPlay={false}/>
-      {/* </div> */}<h1>Installation</h1>
->>>>>>> 02e91dd7fd48ddc04aebe3f2d2c093ef2e77225e
-      <SyntaxHighlighter language='bash' style={coldarkDark}>
+      <SyntaxHighlighter language="bash" style={coldarkDark}>
         {`npm install @madzadev/image-slider`}
       </SyntaxHighlighter>
       <h1>Usage</h1>
-      <SyntaxHighlighter language='javascript' style={coldarkDark}>
+      <SyntaxHighlighter language="javascript" style={coldarkDark}>
         {`import Slider from '@madzadev/image-slider'`}
       </SyntaxHighlighter>
-      <SyntaxHighlighter language='javascript' style={coldarkDark}>
+      <SyntaxHighlighter language="javascript" style={coldarkDark}>
         {`const images = [
           {
             url: 'https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3',
@@ -87,19 +64,19 @@ const App = () => {
           }
         ]`}
       </SyntaxHighlighter>
-      <SyntaxHighlighter language='jsx' style={coldarkDark}>
+      <SyntaxHighlighter language="jsx" style={coldarkDark}>
         {`<Slider imageList={images} />`}
       </SyntaxHighlighter>
-      <p className='warning'>
+      <p className="warning">
         <code>'imageList'</code> is the mandatory prop and requires to pass in
-        an array consisting of objects with <code>url</code>, <code>title</code>{' '}
+        an array consisting of objects with <code>url</code>, <code>title</code>{" "}
         and <code>tags</code> keys.
       </p>
       <h1>Options</h1>
-      <p className='note'>
+      <p className="note">
         The default values of available options props are displayed.
       </p>
-      <SyntaxHighlighter language='jsx' style={coldarkDark}>
+      <SyntaxHighlighter language="jsx" style={coldarkDark}>
         {`<Slider
         imageList={images}
         autoPlay={true}
@@ -114,38 +91,38 @@ const App = () => {
       Volume control Clickable playlist Filter audio files based on genre Search
       audio files by title Responsive design */}
       <h1>Sizing</h1>
-      <p className='note'>
+      <p className="note">
         By default image slider is set to be 100% width and height of it's
         parent. To set specific values, wrap it:
       </p>
-      <SyntaxHighlighter language='jsx' style={coldarkDark}>
+      <SyntaxHighlighter language="jsx" style={coldarkDark}>
         {` <div style={{width: '700px', height: '350px'}}>
       <Slider imageList={images}/>
     </div>`}
       </SyntaxHighlighter>
-      <p className='warning'>
-        Larger images will fit and be centered, thanks to{' '}
+      <p className="warning">
+        Larger images will fit and be centered, thanks to{" "}
         <code>object-fit: cover</code>.
       </p>
-      <p className='warning'>
+      <p className="warning">
         Smaller images will not be stretched, instead the background color will
         be displayed.
       </p>
       <h1>Preview</h1>
-      <p className='note'>
+      <p className="note">
         By default image preview is disabled (dots are used instead). To enable
         it, set <code>preview</code> prop to <code>true</code>.
       </p>
-      <SyntaxHighlighter language='jsx' style={coldarkDark}>
+      <SyntaxHighlighter language="jsx" style={coldarkDark}>
         {`<Slider imageList={images} preview={true}/>`}
       </SyntaxHighlighter>
       <h1>Final notes</h1>
-      <p className='warning'>
+      <p className="warning">
         The project is under MIT licence, so be free to check it out and give
         contributions.
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,78 +1,67 @@
-import React, { useState, useEffect } from 'react'
-import styles from './styles.module.css'
+import React, { useState, useEffect } from "react";
+import styles from "./styles.module.css";
 
-<<<<<<< HEAD
-import Credits from './components/Credits'
-import left from './public/left.svg'
-import right from './public/right.svg'
+var backButton = require("../public/back.svg");
+var nextButton = require("../public/next.svg");
+
+import Credits from "./components/Credits";
 
 export const Slider = ({
   imageList,
+  autoPlay = true,
   showCredits = true,
   showProgress = true,
   showControls = true,
-  showDescription = true
+  showDescription = true,
 }) => {
-=======
-import backButton from './../public/back.png'
-import nextButton from './../public/next.png'
-
-export const Slider = ({ imageList, autoPlay=true, showCredits=true, showProgress=true, showControls=true, showDescription=true}) => {
->>>>>>> 02e91dd7fd48ddc04aebe3f2d2c093ef2e77225e
   // const [array, setArray] = useState([1, 2, 3])
-  let [active, setActive] = useState(0)
+  let [active, setActive] = useState(0);
 
-const setPreviousImage = ()=>{
-  if (active !== 0) {
-    setActive((active -= 1))
-  } else {
-    setActive((active = imageList.length - 1))
-  }
-}
+  const setPreviousImage = () => {
+    if (active !== 0) {
+      setActive((active -= 1));
+    } else {
+      setActive((active = imageList.length - 1));
+    }
+  };
 
-const setNextImage = ()=>{
-  if (active !== imageList.length - 1) {
-    setActive((active += 1))
-  } else {
-    setActive((active = 0))
-  }
-}
+  const setNextImage = () => {
+    if (active !== imageList.length - 1) {
+      setActive((active += 1));
+    } else {
+      setActive((active = 0));
+    }
+  };
 
   const leftClickHandle = () => {
     setPreviousImage();
 
     // const [first, second, ...rest] = array
     // setArray([...rest, first, second])
-  }
+  };
 
   const rightClickHandle = () => {
     setNextImage();
 
     // const [first, ...rest] = array
     // setArray([...rest, first])
-  }
+  };
 
   const dotClickHandler = (e) => {
-    const dotNum = e.target.getAttribute('data-key')
-    setActive((active = parseInt(dotNum)))
-  }
+    const dotNum = e.target.getAttribute("data-key");
+    setActive((active = parseInt(dotNum)));
+  };
 
-  
   useEffect(() => {
-    if(autoPlay){
+    if (autoPlay) {
       let autoSlider = setInterval(setNextImage, 3000);
       return () => clearInterval(autoSlider);
     }
-<<<<<<< HEAD
-  }, 3000)
+  }, [active]);
+
   return (
     <div>
       <div className={styles.wrapper}>
-        {/* {showDescription && (
-          <div className={styles.description}>
-            <p>{imageList[active].title}</p>
-          </div>
-        )} */}
         {/* {showProgress && (
           <p className={styles.progress}>
             {`${active + 1} / ${imageList.length}`}
@@ -80,62 +69,25 @@ const setNextImage = ()=>{
         )} */}
         {showControls && (
           <div className={styles.leftClick} onClick={leftClickHandle}>
-            <img src={left} alt='img' />
+            <img className={styles.button} src={backButton} alt="back" />
           </div>
         )}
-
-        <img src={imageList[active].url} alt='image' />
+        <img src={imageList[active].url} className={styles.image} alt="image" />
         {showControls && (
           <div className={styles.rightClick} onClick={rightClickHandle}>
-            <img src={right} alt='image' />
+            <img className={styles.button} src={nextButton} alt="next" />
           </div>
-=======
-  }, [active]);
-
-  return (
-    <div>
-      {showCredits&&(
-        <div style={{ textAlign: 'right', maxWidth: '1000px' }}>
-        <p>
-          Photo by{' '}
-          <a href='https://unsplash.com/@flyd2069?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>
-            FLY:D
-          </a>{' '}
-          on{' '}
-          <a href='/collections/4390214/abstract?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>
-            Unsplash
-          </a>
-        </p>
-      </div>
-      )}
-      <div className={styles.wrapper}>
-        {showProgress&&(
-          <p className={styles.progress}>
-          {`${active + 1} / ${imageList.length}`}
-        </p>
         )}
-        {showControls&&(
-          <div className={styles.leftClick} onClick={leftClickHandle}>
-            <img className={styles.button} src={backButton} alt="back"/>
-        </div>
-        )}
-        <img src={imageList[active].url} alt='image' />
-        {showControls&&(
-          <div className={styles.rightClick} onClick={rightClickHandle}>
-          <img className={styles.button} src={nextButton} alt="next"/>
-        </div>
-        )}
-        {showDescription&&(
+        {/* {showDescription && (
           <div className={styles.description}>{imageList[active].title}</div>
->>>>>>> 02e91dd7fd48ddc04aebe3f2d2c093ef2e77225e
-        )}
+        )} */}
       </div>
-      {showCredits && (
+      {/* {showCredits && (
         <Credits
           author={imageList[active].author}
           authorLink={imageList[active].authorLink}
         />
-      )}
+      )} */}
 
       <div className={styles.dots}>
         {imageList.map((el, index) => {
@@ -147,12 +99,12 @@ const setNextImage = ()=>{
                 data-key={index}
                 onClick={dotClickHandler}
               />
-            )
+            );
           } else {
-            return <div className={styles.activeDot}></div>
+            return <div className={styles.activeDot}></div>;
           }
         })}
       </div>
     </div>
-  )
-}
+  );
+};
